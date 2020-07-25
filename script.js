0c26e14a7325f5f0987cbb748f0a5b003065c8a7
joao victor <jaotipereira@gmail.com>
2020-07-25 13:19:59 -0300

    wip:dialog modife size

---
+++ script.js
@@ -0,0 +1,5 @@
+var cssLink = document.createElement("link");
+cssLink.href = "./css/style.css";  
+cssLink.rel = "stylesheet";  
+cssLink.type = "text/css";  
+frames.document.body.appendChild(cssLink);
