478f65803b85c3bdfabda02688d5c8d116acc766
joao victor <jaotipereira@gmail.com>
2020-07-25 17:10:50 -0300

    wip: button show

---
+++ js/script.js
@@ -0,0 +1,8 @@
+$( document ).ready(function() {
+    console.log( "ready!" );
+    document.getElementsByTagName("iframe")[1].style.display = "none";
+    document.getElementsByTagName("iframe")[1].document.getElementById("u_0_0").style.display = "block";
+    document.getElementsByTagName("iframe")[1].document.getElementsByClassName("_f_0")[0].style.display = "none";
+});
+
+
